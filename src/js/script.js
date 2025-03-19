59ee1dedf6336860ae173085f813989cdadc9a8f
TruongChau <truongcp2105@gmail.com>
2025-03-19 12:10:53 +0700

    modified index.html

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let passwordToggles = document.querySelectorAll(".toggle-password");
+
+//     passwordToggles.forEach(toggle => {
+//         toggle.addEventListener("click", function () {
+//             let inputField = this.previousElementSibling; // Lấy input password trước nó
+//             if (inputField.type === "password") {
+//                 inputField.type = "text";
+//                 this.textContent = "🙈"; // Đổi icon thành con mắt đóng
+//             } else {
+//                 inputField.type = "password";
+//                 this.textContent = "👁️"; // Đổi icon thành con mắt mở
+//             }
+//         });
+//     });
+// });
