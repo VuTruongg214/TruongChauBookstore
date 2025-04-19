9496881547b3a54a06113e13a2186621a309c750
baka <anhhn.23810320332@gmail.com>
2025-04-19 14:13:05 +0700

    hoàn thành page 3

---
+++ src/static/js/tintuc.js
@@ -0,0 +1,145 @@
+const newsData = [
+    {
+        id: 1,
+        tieude: "Hoạt động đón Ngày Sách và Văn hóa đọc năm 2025 tại Lâm Đồng",
+        ngay:"17/4/2025",
+        nd1: "Trong tháng 4, tại Đà Lạt (Lâm Đồng) diễn ra nhiều hoạt động hưởng ứng Ngày Sách và Văn hóa đọc Việt Nam năm 2025.",
+        nd2: `Ngày 20/4, tại Trung tâm hoạt động thanh thiếu nhi Lâm Đồng diễn ra buổi giao lưu, gặp gỡ nhà thơ Trần Đăng Khoa (sáng) và tác giả Nguyễn Vĩnh Nguyên (chiều).
+
+        Từ ngày 2 đến 20/4, nơi đây cũng tổ chức cuộc thi "Vẽ tranh theo sách - Sắc màu em yêu"; cuộc thi Giới thiệu sách trực tuyến năm 2025 và trưng bày, triển lãm ảnh.
+
+        Trong hai ngày 19-20/4, khuôn viên Trung tâm có không gian "Thư viện xanh - Con đường sách" nơi độc giả có thể mua sách tranh với giá ưu đãi từ các đơn vị chuyên sách thiếu nhi như Crabit Kidbooks, Slowbooks và Ô Cửa Sách. Nhân dịp này, Ô Cửa Sách cũng dành tặng 1.000 cuốn sách cho học sinh.
+
+        Trong hai ngày 16-17/4 tại Thư viện Lâm Đồng diễn ra các buổi giới thiệu sách, giao lưu với tác giả các bộ sách thiếu nhi trong dự án "Khi trẻ là tác giả của" của Ô Cửa Sách: Chuyến đi của Gấu Nâu (sáng 17); Đám trẻ ở vùng đất sương mù, Thư viện kỳ bí và Bạn không cần nhìn thấy, mùa xuân vẫn tràn đầy (chiều 17).
+
+        Từ ngày 2/4 đến ngày 2/5, Thư viện Lâm Đồng cũng tổ chức chương trình cấp thẻ đọc sách, mượn sách miễn phí cho độc giả. Ngoài ra, chương trình xe thư viện lưu động và luân chuyển sách qua các địa bàn Đà Lạt, Bảo Lộc, Lâm Hà, Di Linh, Đạ Huoai cũng diễn ra từ ngày 1/4 đến 15/5.
+
+        Từ năm 2014, ngày 21/4 được chọn làm Ngày Sách Việt Nam và năm 2017 được đổi tên thành Ngày Sách và Văn hóa đọc Việt Nam, đến nay hàng năm được nhiều địa phương trên cả nước hưởng ứng.`,
+
+    },
+    {
+        id: 2,
+        tieude: "NXB Giáo dục Việt Nam và Bộ Giáo dục Đại học Cuba hợp tác xuất bản",
+        ngay:"15/4/2025",
+        nd1: "Ngày 14/4/2025, tại Hà Nội, Nhà xuất bản Giáo dục Việt Nam (NXBGDVN) và Bộ Giáo dục Đại học Cuba đã ký kết thỏa thuận hợp tác trong lĩnh vực xuất bản, in sách và phát triển văn hóa đọc.",
+        nd2: `Sự kiện đánh dấu sự thúc đẩy và tăng cường hợp tác giữa NXBGDVN và Bộ Giáo dục Đại học nước Cộng hòa Cuba.
+        
+        Tham dự buổi lễ, về phía NXBGDVN có: ông Nguyễn Tiến Thanh - Chủ tịch Hội đồng thành viên (HĐTV), Tổng giám đốc; ông Phạm Vĩnh Thái - Thành viên HĐTV, Tổng biên tập; ông Phạm Văn Thắng - Thành viên HĐTV; ông Nguyễn Văn Tùng - Thành viên HĐTV, Phó tổng Biên tập; ông Nguyễn Chí Bính - Phó tổng Giám đốc; bà Phùng Ngọc Hồng - Phó tổng Giám đốc; ông Đặng Thanh Hải - Phó tổng Giám đốc; ông Trần Đức Hùng - Kế toán trưởng và đại diện các phòng, ban chuyên môn.
+
+        Về phía Bộ Giáo dục Đại học Cuba, đại diện có: ông Reynaldo Velázquez Zaldívar - Thứ trưởng; bà Iliana Martinez - Vụ trưởng Vụ Hợp tác Quốc tế.
+
+        Về phía Đại sứ quán nước Cộng hòa Cuba tại Việt Nam, đại diện có: ông Rogelio Polanco Fuentes - Đại sứ đặc mệnh toàn quyền nước Cộng hòa Cuba tại Việt Nam; bà Maylem Rivero Silva - Tham tán Công sứ Phụ trách Văn phòng Kinh tế và Thương mại và một số thành viên khác trong đoàn.
+
+        Lễ ký kết giữa NXBGDVN với Bộ Giáo dục Đại học Cuba được thực hiện với mong muốn thúc đẩy và tăng cường hợp tác trong lĩnh vực xuất bản, in sách và phát triển văn hóa đọc trên cơ sở các nguyên tắc bình đẳng và cùng có lợi; với mục tiêu phát triển quan hệ hợp tác toàn diện và tăng cường hơn nữa mối quan hệ hữu nghị bền chặt giữa hai nước.
+
+        Theo nội dung thỏa thuận, NXBGDVN và Bộ Giáo dục Đại học Cuba sẽ trao đổi thông tin và chia sẻ kinh nghiệm trong các lĩnh vực hai bên cùng quan tâm; tư vấn cho đội ngũ chuyên gia và kỹ thuật viên của các nhà xuất bản giáo dục đại học tại Cuba và Việt Nam; xây dựng và triển khai các chương trình, dự án hợp tác chung; tham gia các hội nghị, hội thảo, diễn đàn và sự kiện do mỗi bên tổ chức; biên tập, in ấn các ấn phẩm có nội dung phù hợp…
+
+        Trong khuôn khổ Đại hội Quốc tế Universidad 2026, Bộ Giáo dục Đại học Cuba trân trọng mời NXBGDVN tham gia với tư cách đơn vị trưng bày sách. Đồng thời, theo đề nghị bên phía Cuba, NXBGDVN sẽ tổ chức biên tập, in ấn hai cuốn sách nhằm kỷ niệm 50 năm thành lập Bộ Giáo dục Đại học Cuba và 100 năm ngày sinh của lãnh tụ vĩ đại Cách mạng Cuba, Fidel Castro Ruz. Bộ Giáo dục Đại học Cuba sẽ cung cấp bản thảo và bản quyền của các cuốn sách. Các cuốn sách sẽ được xuất bản bằng hai ngôn ngữ Tiếng Tây Ban Nha và Tiếng Việt.
+
+        Đối với Cuba - một đất nước có mối quan hệ hữu nghị đặc biệt với Việt Nam, chúng tôi luôn trân trọng những tình cảm trong lịch sử của hai nước; ghi nhớ, biết ơn sâu sắc sự giúp đỡ của nhân dân và đất nước Cuba đối với Việt Nam trong những giai đoạn kháng chiến khó khăn nhất. Vì vậy, việc giúp đỡ, hợp tác, ủng hộ Cuba không chỉ là trách nhiệm, nghĩa vụ mà còn xuất phát từ trái tim, lòng biết ơn của NXBGDVN đối với nhân dân Cuba…”
+
+        Trong bài phát biểu của mình, ông Reynaldo Velázquez Zaldívar - Thứ trưởng Bộ Giáo dục Đại học Cuba cũng nhấn mạnh: “Năm 2025 là một năm có sự tham gia với trọng trách lớn hơn của ngành giáo dục Cuba đối với công cuộc đào tạo nhân sự cho sự phát triển kinh tế của đất nước.
+
+        Hệ thống các trường đại học tại Cuba gồm 50 trường với khoảng 250.000 sinh viên theo học, do đó lượng sách về giáo dục được sử dụng tại Cuba rất là lớn. Tuy nhiên, tình hình kinh tế của Cuba trong hiện tại rất khó khăn, NXB Đại học Cuba mất khả năng in ấn sách cho ngành giáo dục. Vì vậy, chúng tôi tin tưởng rất lớn về tiềm năng hợp tác giữa Việt Nam và Cuba trong việc biên tập, in ấn, hỗ trợ tư vấn trong ngành giáo dục...”
+
+        Ông Reynaldo Velázquez Zaldívar cũng thay mặt đoàn cảm ơn và bày tỏ lòng trân trọng trước sự đón tiếp nồng hậu cũng như sự giúp đỡ của NXBGDVN dành cho Cuba. Ông tin tưởng việc ký kết thỏa thuận hợp tác giữa hai bên sẽ là khởi đầu tốt đẹp cho sự hợp tác lâu dài, hiệu quả giữa NXBGDVN và Bộ Giáo dục Đại học Cuba.
+
+        NXBGDVN cũng tin rằng, việc hợp tác này sẽ là tiền đề phát triển trong lĩnh vực xuất bản và phát triển văn hóa đọc giữa hai nước Việt Nam và Cuba.
+
+        Trước đó, tại La Habana, từ ngày 22-26/02/2025, đoàn cán bộ Nhà xuất bản Giáo dục Việt Nam do Chủ tịch Hội đồng thành viên, Tổng Giám đốc Nguyễn Tiến Thanh dẫn đầu đã tới thăm và làm việc tại Cuba, nhằm tăng cường hợp tác trong lĩnh vực xuất bản giữa Bộ Giáo dục và Đào tạo Việt Nam và Bộ Giáo dục Đại học Cuba. Trong khuôn khổ chuyến công tác này, NXBGDVN đã gửi tặng Bộ Giáo dục Đại học Cuba 30 chiếc máy tính bảng. Món quà cũng chính là tình cảm, là tấm lòng của NXBGDVN đối với các bạn Cuba.`,
+    },
+    {
+        id: 3,
+        tieude: "Khởi công đường sách Nguyễn Đổng Chi, kiến tạo biểu tượng văn hóa mới",
+        ngay:"15/4/2025",
+        nd1: "Sáng 15/4, UBND quận 7 phối hợp cùng Sở Văn hóa và Thể thao TP.HCM tổ chức Lễ khởi công Đường sách Nguyễn Đổng Chi nhân kỷ niệm 50 năm ngày thống nhất đất nước.",
+        nd2: `Đường sách Nguyễn Đổng Chi được xây dựng tại đường Nguyễn Đổng Chi, phường Tân Phú, quận 7 (đoạn từ Nguyễn Văn Linh đến Nguyễn Lương Bằng). Dự án được kỳ vọng trở thành biểu tượng văn hóa mới tại khu đô thị Nam Sài Gòn, hướng đến mô hình không gian đọc tích hợp đa chức năng, hiện đại và gần gũi với cộng đồng.
+
+        Đây không chỉ là nơi trưng bày sách mà còn là điểm đến văn hóa, nơi người dân mọi độ tuổi có thể khám phá tri thức, tham gia hoạt động nghệ thuật, kết nối cộng đồng và trải nghiệm công nghệ hiện đại với quy mô đầu tư gồm 1 thư viện thông minh, 1 khu hỗ trợ khởi nghiệp, cùng 16 gian hàng văn hóa, sáng tạo, ẩm thực, du lịch.
+
+        Một trong những điểm nhấn đáng chú ý của dự án là thư viện thông minh ứng dụng công nghệ VR, AI và IOT, khu vực chuyển đổi số, không gian hỗ trợ khởi nghiệp, sân chơi thiếu nhi, cà phê sách, khu triển lãm văn hóa - du lịch và gian hàng ẩm thực đặc sản. Thiết kế không gian mở, xanh và sáng tạo giúp người dân mọi lứa tuổi dễ dàng tiếp cận tri thức, tham gia hoạt động nghệ thuật và tương tác công nghệ.
+
+        Bên cạnh đó, dự án còn đề cao việc giữ gìn bản sắc văn hóa Việt thông qua các không gian triển lãm, trưng bày nghệ thuật, quảng bá du lịch, đặc sản vùng miền và đồng hành cùng các lễ hội tôn vinh văn hóa Việt.
+
+        Phát biểu tại buổi lễ, ông Châu Xuân Đại Thắng - Quyền chủ tịch UBND quận 7 - nhấn mạnh: “Đường sách Nguyễn Đổng Chi là một công trình văn hóa mang tầm chiến lược, đánh dấu bước chuyển mình mạnh mẽ trong hành trình kiến tạo không gian văn hóa đọc hiện đại, sáng tạo và mang bản sắc riêng tại khu vực đô thị phía Nam thành phố”.
+
+        Với định hướng “Kết nối yêu thương - Kiến tạo giá trị nhân văn”, công trình đặt trọng tâm vào phát triển bền vững, thúc đẩy giáo dục, lan tỏa tri thức và nâng cao chất lượng sống cho cộng đồng.
+
+        Ông Trần Thế Thuận - Giám đốc Sở Văn hóa và Thể thao TP.HCM - cho biết: “Đường sách Nguyễn Đổng Chi là một phần của chiến lược mở rộng mạng lưới không gian sách trên địa bàn TP.HCM tiếp nối thành công của các mô hình đường sách tại TP.HCM và Thủ Đức”.
+
+        Trả lời phỏng vấn của Znews, ông Trần Văn Ly - Giám đốc Trung tâm Văn hóa và Thể thao quận 7, cũng là chủ đầu tư dự án đường sách Nguyễn Đổng Chi - chia sẻ rằng dự án này đã được ban lãnh đạo ấp ủ suốt 7 năm với kỳ vọng tạo ra một không không gian văn hóa mở, thân thiện, gần gũi để phục vụ nhu cầu đa dạng của người dân thành phố, đồng thời tạo dấu ấn với bạn bè quốc tế vì quận 7 là nơi có hơn 15.000 người nước ngoài đang sinh sống và làm việc.
+
+        Công trình dự kiến hoàn thành và đưa vào hoạt động dịp kỷ niệm 80 năm Quốc khánh nước Cộng hòa xã hội chủ nghĩa Việt Nam (2/9/2025), được kỳ vọng trở thành biểu tượng văn hóa mới của khu Nam Sài Gòn, đóng góp vào sự phát triển bền vững của thành phố.
+
+        Song song với việc khởi công công trình tại quận 7, Sở Văn hóa và Thể thao cũng lên kế hoạch tổ chức Lễ khánh thành Đường sách Bình Tân vào ngày 25/4 và Không gian sách Củ Chi trong thời gian tới, hình thành mạng lưới không gian đọc toàn diện trên địa bàn TP.HCM lấy Đường sách TP.HCM (đường Nguyễn Văn Bình, quận 1) làm trung tâm và có 4 không gian sách "vệ tinh" đủ các tọa độ nam, bắc, đông, tây.`,
+    },
+    {
+        id: 4,
+        tieude:"Fahasa lọt top doanh nghiệp tiêu biểu nhân 50 năm thống nhất đất nước",
+        ngay:"15/4/2025",
+        nd1:"Fahasa được chọn là 1 trong 50 doanh nghiệp, đơn vị tiêu biểu có thương hiệu và sản phẩm chủ lực của thành phố, nhân kỷ niệm 50 năm thống nhất đất nước.",
+        nd2:`Công ty CP Phát hành Sách TP.HCM (Fahasa) là doanh nghiệp duy nhất trong lĩnh vực xuất bản - phát hành góp mặt trong danh sách 50 đơn vị được công nhận có thương hiệu và sản phẩm chủ lực của TP.HCM nhân kỷ niệm 50 năm thống nhất đất nước (30/4/1975 - 30/4/2025).
+
+        Danh sách này còn bao gồm các doanh nghiệp khác từ đa dạng lĩnh vực như cơ khí, sản xuất lương thực, thực phẩm, dệt may, logistics,... cùng một số trường đại học, bệnh viện.
+
+        Năm 1976, UBND TP.HCM quyết định thành lập Quốc doanh phát hành sách, thuộc sở Thông tin - Văn hóa, lần lượt đổi tên thành Công ty Phát hành sách TP.HCM (1982) và từ 2005 đến nay là Công ty CP Phát hành sách (Fahasa).
+
+        Quá trình phát triển của Fahasa trải qua 4 giai đoạn chính. Năm 1976-1985 có 6 cửa hàng sách chuyên đề và 18 hiệu sách tại 18 quận, huyện nội ngoại thành. Giai đoạn chuyển đổi sang nền kinh tế thị trường năm 1986-1990 ngành phát hành sách cả nước thua lỗ kéo dài, nhiều đơn vị trong ngành phải đóng cửa; Công ty Phát hành sách TP.HCM cũng gặp không ít khó khăn.
+
+        Năm 1990-2004 công ty có một số giải pháp, hướng đi thử nghiệm mới như liên doanh liên kết với các đơn vị trong và ngoài ngành, mở rộng ra nhiều quận ven và tỉnh lân cận. Cả nước trên đà phát triển nên nhu cầu và sức mua cũng nâng lên. Đơn vị mở ra phong cách phục vụ mới: gian hàng sách tự chọn. Hàng hóa đa dạng, phong phú hơn.
+
+        Từ năm 2005 đến nay, Fahasa bước vào giai đoạn phát triển ổn định bền vững, thích nghi được trước những biến động thị trường như xu hướng mua sắm trực tuyến, nhu cầu và thói quen của độc giả trong thế kỷ 21 và liên tục mở rộng, làm mới hệ thống nhà sách. Tính đến tháng 12/2024, công ty có hệ thống 130 nhà sách trên cả nước, trong đó 34 nhà sách ở TP.HCM và 96 nhà sách tại các tỉnh thành khác.
+
+        Suốt gần 50 năm hoạt động, đơn vị từng được trao tặng nhiều danh hiệu như Huân chương Lao động hạng Nhì năm 2000, hạng Nhất năm 2006; Hàng Việt Nam Chất lượng cao do người tiêu dùng bình chọn; Cờ thi đua xuất sắc ngành phát hành sách; chứng nhận Doanh nghiệp có hệ thống Nhà sách nhiều nhất vượt kỷ lục Việt Nam từ trung tâm sách Kỷ lục Việt Nam; Giải thưởng thương mại dịch vụ Việt Nam; Giải thưởng Sao Vàng Đất Việt; Giải thưởng Sao Vàng Phương Nam...
+
+        Doanh nghiệp 4 lần lọt Top 500 Nhà bán lẻ hàng đầu châu Á - Thái Bình Dương, Top 10 Nhà bán lẻ hàng đầu Việt Nam do Tạp chí Retail Asia (Singapore) bình chọn.`,
+    },
+    {
+        id: 5,
+        tieude:"Sách mới của Thomas Pynchon xuất bản vào tháng 10",
+        ngay:"14/4/2025",
+        nd1:"Tác giả người Mỹ 87 tuổi, nổi tiếng với tiểu thuyết 'V.', sẽ trở lại văn đàn sau 12 năm kể từ tác phẩm 'Bleeding Edge' xuất bản năm 2013.",
+        nd2:`Tiểu thuyết Shadow Ticket là tác phẩm thứ 10 của ông, dự kiến phát hành ngày 7/10, sẽ mang nhiều yếu tố đặc trưng văn chương Pynchon - hoang tưởng, gián điệp, âm hưởng nhạc kịch với những nhân vật lập dị, giàu chiều sâu hơn vẻ ngoài, gặp phải những tình huống tiến thoái lưỡng nan, theo New York Times.
+
+        Lấy bối cảnh năm 1932 thời kỳ Đại suy thoái, cuốn sách kể về thám tử tư người Milwaukee tên Hicks McTaggart. Anh được giao nhiệm vụ tìm kiếm người thừa kế bỏ trốn của đế chế pho mát Wisconsin. Nhiệm vụ tưởng chừng tầm thường này ngày một "khoai" hơn khi rốt cuộc McTaggart lên con tàu biển xuyên Đại Tây Dương rồi đến Hungary.
+
+        Trên hành trình truy đuổi người thừa kế gian trá, anh bị cuốn vào mạch ngầm chính trị đang khuấy động Đông Âu - rắc rối với Đức Quốc xã, điệp viên Liên Xô và điệp viên phản gián Anh, cả những nhạc sĩ swing, những tội phạm đua xe, và những kẻ thực hành các nghi thức huyền bí.
+
+        Pynchon ghi dấu như một tên tuổi lớn trên văn đàn với tiểu thuyết thứ ba Gravity's Rainbow, kiệt tác siêu hình hậu hiện đại đồ sộ lấy bối cảnh châu Âu sau Thế chiến II, xuất bản năm 1973 và giành Giải thưởng Sách quốc gia Mỹ. Những thập kỷ tiếp theo, ông tiếp tục khiến giới phê bình và độc giả choáng ngợp với những tác phẩm đậm đặc, hài hước, lập dị như Vineland, Mason & Dixon và Against the Day.
+
+        Tuy nhiên, sau hơn sáu thập kỷ vang danh văn đàn, ở tuổi 87, Pynchon vẫn cực kỳ kín tiếng, tránh né phỏng vấn và xuất hiện trước công chúng. Hiếm khi người ta chụp được ảnh ông.
+
+        Ông từ chối cái mác "ẩn dật". Dạo nọ một đoàn phim quay ông ở Manhattan mà chưa xin phép, khiến ông phàn nàn rằng báo chí dùng từ này khi khó chịu vì ai đó không tiếp chuyện họ. Thỉnh thoảng, ông châm biếm cái tiếng yếm thế của mình, góp giọng thoáng qua trong The Simpsons, thô lỗ hét vào điện thoại di động với chiếc túi giấy trùm đầu.
+
+        Danh tính "mập mờ" của ông khiến các học giả và nhà báo thất vọng song chỉ khiến văn chương của ông thêm phần hấp dẫn và bí ẩn. Người ta ngạc nhiên khi năm 2022 Pynchon bán toàn bộ văn thư của mình cho Thư viện Huntington ở San Marino, California, bao gồm ghi chú nghiên cứu, bản đánh máy và bản nháp, thư từ xuất bản, nhưng không có thư từ cá nhân hay ảnh chụp.
+
+        Shadow Ticket có thể lần nữa khơi dậy những tò mò và suy đoán mới về đời tư và di sản của Pynchon. Thông cáo báo chí của cuốn sách có vẻ đáng ngờ như thể do chính Pynchon viết. Nhà xuất bản Penguin đã xác nhận điều này. “Vây quanh là cái lịch sử mà anh không tài nào nắm bắt, cũng chẳng thấy lối vào đường ra, ánh sáng le lói duy nhất của Hicks chỉ là buổi bình minh của Kỷ nguyên Big Band còn anh tình cờ lại là một vũ công khá cừ. Liệu điều này có đủ giúp anh xoay xở nhảy lindy-hop về lại Milwaukee và thế giới bình thường, nơi có lẽ chẳng còn tồn tại nữa, đó lại là một câu hỏi khác”, trích từ lời giới thiệu của chính Pynchon.
+        `,
+    },
+    {
+        id: 6,
+        tieude:"Cựu Bí thư Hà Nội tặng TP.HCM sách nhật ký chiến trường",
+        ngay:"13/4/2025",
+        nd1:"Nhân kỷ niệm 50 năm thống nhất đất nước, ông Phạm Quang Nghị đã tái bản và in sách để gửi tặng các địa phương đã sống và chiến đấu.",
+        nd2:`Ông chia sẻ cuốn sách là kỷ niệm riêng của bản thân, nhưng cũng là kỷ niệm chung của biết bao người đã sống trong thời gian chiến tranh.
+
+        Sáng 12/4, Ban Tuyên giáo và Dân vận Thành ủy TP.HCM tổ chức buổi lễ trao tặng sách Nơi ấy là chiến trường” của tác giả Phạm Quang Nghị, nguyên Ủy viên Bộ Chính trị, nguyên Bí thư Thành ủy Hà Nội, nhân kỷ niệm 50 năm ngày thống nhất đất nước (30/4/1975-30/4/2025).
+
+        Cuốn sách Nơi ấy là chiến trường được ông Phạm Quang Nghị tổng hợp từ những ghi chép suốt quá trình đi dọc Trường Sơn và khắp các chiến trường trong thời gian tác giả tham gia cách mạng.
+
+        Những trang nhật ký được tác giả ghi đều mỗi ngày trong những khoảnh khắc có phần "không bình thường", từ đường hành quân, hay trong những ngày lặng im tiếng súng cho đến những trang viết ra đời giữa làn đạn sáng lòe trong đêm hoặc trong lúc cơn sốt rét đang hành hạ bản thân.
+
+        Tác giả chọn mốc thời gian mở đầu cuốn sách là từ ngày lên đường ra trận. Có gần 100 trang được ghi công phu cùng nhiều tư liệu quý trong thời gian tác giả học khóa IV Hội Nhà văn Việt Nam và những ngày huấn luyện, tập tành súng ống... để chuẩn bị vượt Trường Sơn.
+
+        Ông Phạm Quang Nghị bày tỏ qua tác phẩm ông muốn gửi lời tri ân đồng bào, chiến sĩ nhân dân đã cùng sống, chiến đấu và được nhân dân nuôi giấu, giúp đỡ.
+
+        Ông Nghị cho biết bản thân có thói quen ghi nhật ký và may mắn đã tìm được hết các cuốn nhật ký. Sau ngày đất nước thống nhất, ông đã tổng hợp lại để hoàn thành tác phẩm từ cuốn nhật ký và ghi chép suốt những năm tháng đi B. Cuốn Nơi ấy là chiến trường phát hành lần đầu năm 2019 và được đón nhận nồng nhiệt.
+
+        Nhân kỷ niệm 50 năm thống nhất đất nước, với sự hỗ trợ của Hội Di sản văn hóa Việt Nam, ông Phạm Quang Nghị đã tái bản và in sách để gửi tặng các địa phương đã từng sống và chiến đấu. Cuốn sách mặc dù là kỷ niệm riêng của bản thân, nhưng cũng là kỷ niệm chung của biết bao người đã sống trong thời gian chiến tranh.
+
+        Phát biểu tại buổi lễ, Trưởng ban Tuyên giáo và Dân vận Thành ủy TP.HCM Nguyễn Mạnh Cường bày tỏ vinh dự khi đón nhận món quà ý nghĩa của ông Phạm Quang Nghị dành tặng Thành ủy TPHCM, Ban Tuyên giáo và Dân vận Thành ủy TP.HCM. Đây là món quà quý giá, rất ý nghĩa để thực hiện công tác tuyên truyền của thành phố, đặc biệt trong dịp kỷ niệm 50 năm ngày thống nhất đất nước.
+
+        Theo ông Cường, qua cuốn sách, độc giả cảm nhận sâu sắc hơn ý nghĩa cuộc đấu tranh giải phóng đất nước, cảm nhận quý giá về hòa bình của đất nước. Đây là những dòng viết đầy chân thật, từ việc lưu giữ những ghi chép nay trở thành cuốn sách của thế hệ đi B.
+        `,
+    }
+];
